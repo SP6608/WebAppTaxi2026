e43a873a53b29d08411951f8d3ec3fd53f1d047b
SP6608 <spapanov@gmail.com>
2026-02-13 23:44:36 +0200

    Добавям бутони във All View -> Edit, Delete и Details

---
+++ WebAppTaxi2026/ViewModels/TaxServiceDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppTaxi2026.ViewModels
+{
+    public class TaxServiceDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Дата и час")]
+        public DateTime HireDateTime { get; set; }
+
+        [Display(Name = "Престой (минути)")]
+        public int DownTime { get; set; }
+
+        [Display(Name = "Изминати километри")]
+        public double TraveledKm { get; set; }
+
+        [Display(Name = "Автомобил")]
+        public string CarInfo { get; set; } = null!;
+    }
+}
